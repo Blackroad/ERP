1ac49d35fbd7b11ecc164483ff4f314f4db98e55
Blackroad <VladVell@gmail.com>
2019-03-14 19:33:48 +0200

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from fixture.application import Application
+import json
+import os.path
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),file)
+        with open(config_file) as opened_file:
+            target = json.load(opened_file)
+    return target
+
+
+@pytest.fixture(scope='session')
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture(scope='session')
+def app(request,config):
+    global fixture
+    browser = request.config.getoption("--browser")
+    if fixture is None:
+        fixture = Application(browser= browser, config = config)
+    return fixture
+
+
+@pytest.fixture()
+def stop(request):
+    def fin():
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+def pytest_addoption(parser):
+    parser.addoption ("--browser", action='store', default='chrome')
+    parser.addoption ("--target", action='store', default='target.json')
